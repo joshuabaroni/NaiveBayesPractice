0ff39b01849ae7292246795a5549fe6573796884
Joshua Baroni <joshua.baroni.16@cnu.edu>
2020-08-05 19:44:04 -0400

    Heroku Debugging

--- src/utilities/Utils.java
+++ src/utilities/Utils.java
@@ -1,6 +1,6 @@
 package utilities;
 
 public class Utils {
-    public static final String FILESPACE = "C:/Users/joshu/Documents/NaiveBayesPractice/src/testdata";
+    public static final String FILESPACE = "./src/testdata";
     public static final String[] SUPPORTED_FILETYPES = {"csv", "arff"};
 }
