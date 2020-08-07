fc2834850cc1cefa7d18efdc64cc3290fd2eb28e
Joshua Baroni <joshua.baroni.16@cnu.edu>
2020-08-06 20:26:58 -0400

    CORS Same-Origin auth granted

--- src/main/NBController.java
+++ src/main/NBController.java
@@ -8,12 +8,15 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import models.BasicNaiveBayes;
 
+@CrossOrigin(origins="http://localhost:3000")
 @RestController
 public class NBController {
 	
